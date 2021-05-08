064f337d5c287c0bb4b4bbca65e7dcffd9248e80
Ahmet KARABUDAK <ahmet.karabudakk.9122@gmail.com>
2021-05-08 23:08:14 +0300

    Context Class Created

---
+++ DataAccessLayer/Concrate/Context.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrate
+{
+    public class Context : DbContext
+    {
+
+        //Context Sınıfındaki About-Category vb gibi sınıfları çağırmak için entityLayer kısmını referans olarak gösterdik
+        //Context Sınıfının amacı buraya yazılan Dbset türündeki propertyleri SQL e tablo olarak yansıtır
+        //About bu proje içindeki sınıfın ismi 's' takısı almış olan Abouts ise SQL deki tablonun ismi.
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category>  Categories { get; set; }
+        public DbSet<Contact>   Contacts { get; set; }
+        public DbSet<Content>   Contents  { get; set; }
+        public DbSet<Heading>   Headings  { get; set; }
+        public DbSet<Writer>    Writers  { get; set; }
+
+    }
+}
