632a4bd955e04afbdea2689dc32ba9fb15dccc8a
Ahmet KARABUDAK <ahmet.karabudakk.9122@gmail.com>
2021-05-09 15:47:53 +0300

    Abstract and Interface component

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal
+    {
+        //CRUD
+        /*
+        Creat
+        Read
+        Update
+        Delete
+        */
+        //Type Name();
+
+        List<Category> List();
+        void Insert(Category p);
+        void Update(Category p);
+        void Delete(Category p);
+    }
+}
