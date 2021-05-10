82229698222eb16b6940915a58df078cb645ba0e
Ahmet KARABUDAK <ahmet.karabudakk.9122@gmail.com>
2021-05-10 17:14:43 +0300

    Listing of Categories

--- MVC-PROJE/Controllers/CategoryController.cs
+++ MVC-PROJE/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,17 @@ namespace MVC_PROJE.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryManager cm = new CategoryManager();
         // GET: Category
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            //var diyoruz çünkü tabloda alfabetik karakter sayı ve değişik karakterler işçerebilir oyüzden bunu kullanıyoruz.
+            var categoryvalues = cm.GetAllBL();
+            return View(categoryvalues);
         }
     }
 }
