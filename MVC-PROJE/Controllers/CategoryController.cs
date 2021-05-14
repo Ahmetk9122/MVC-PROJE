02aded3f3f63ec566b4347c736864812bc0de84d
Ahmet KARABUDAK <ahmet.karabudakk.9122@gmail.com>
2021-05-14 17:36:55 +0300

    Entity Framework Dal Added.

--- MVC-PROJE/Controllers/CategoryController.cs
+++ MVC-PROJE/Controllers/CategoryController.cs
@@ -5,12 +5,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using DataAccessLayer.EntityFramework;
 namespace MVC_PROJE.Controllers
 {
     public class CategoryController : Controller
     {
-        CategoryManager cm = new CategoryManager();
+        CategoryManager cm = new CategoryManager(new EfContentDal());
         // GET: Category
         public ActionResult Index()
         {
@@ -19,8 +19,8 @@ namespace MVC_PROJE.Controllers
         public ActionResult GetCategoryList()
         {
             //var diyoruz çünkü tabloda alfabetik karakter sayı ve değişik karakterler işçerebilir oyüzden bunu kullanıyoruz.
-          //  var categoryvalues = cm.GetAllBL();
-            return View();
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
         }
         [HttpGet]
         public ActionResult AddCategory()
@@ -31,10 +31,10 @@ namespace MVC_PROJE.Controllers
         [HttpPost]
         public ActionResult AddCategory(Category p)
         {
-          //  cm.CategoryAddBL(p);
+            //  cm.CategoryAddBL(p);
             return RedirectToAction("GetCategoryList");
-            
+
         }
-     
+
     }
 }
