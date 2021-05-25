30f63ea5744386fdf6b9ffbdb76d613afa21f6f2
Ahmet KARABUDAK <ahmet.karabudakk.9122@gmail.com>
2021-05-25 14:11:44 +0300

    WriterValidator added

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrate;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar Adını Boş Geçemezsiniz.");
+            RuleFor(x => x.WriterSurName).NotEmpty().WithMessage("Yazar Soyadı Boş Geçilemez.");
+            RuleFor(x => x.WriterDiscription).MaximumLength(100).WithMessage("Hakkında Kısmını boş Geçemezsiniz");
+            RuleFor(x => x.WriterImage).MaximumLength(100).WithMessage("Resim alanı boş geçilemez.");
+            RuleFor(x => x.WriterPassword).MaximumLength(20).WithMessage("Parola Alanı Boş Geçilemez");
+            RuleFor(x => x.WriterPhone).MaximumLength(20).WithMessage("Telefon alanı Boş Geçilemez");
+
+        }
+    }
+}
