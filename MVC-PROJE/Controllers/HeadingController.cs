d4793ed957269f92cd34213c8f4f8d16f794edb6
Ahmet KARABUDAK <ahmet.karabudakk.9122@gmail.com>
2021-05-26 11:07:47 +0300

    Heaading Controller and Heading view added in MVC-PROJE layer

---
+++ MVC-PROJE/Controllers/HeadingController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_PROJE.Controllers
+{
+    public class HeadingController : Controller
+    {
+        HeadingMenager hm = new HeadingMenager(new EfHeadingDal());
+        // GET: Heading
+        public ActionResult Index()
+        {
+            var headinvalues = hm.GetList();
+            return View(headinvalues);
+        }
+        //public ActionResult  
+    }
+}
