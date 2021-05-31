22cf1cc55957aed5e3892af78047ae251f362763
Ahmet KARABUDAK <ahmet.karabudakk.9122@gmail.com>
2021-05-31 10:24:29 +0300

    Heading deletion in Heading Controller has been adjusted

--- MVC-PROJE/Controllers/HeadingController.cs
+++ MVC-PROJE/Controllers/HeadingController.cs
@@ -68,6 +68,18 @@ namespace MVC_PROJE.Controllers
             ViewBag.vlc = valueCategory;
             return View(HeadingValue);
         }
+        [HttpPost]
+        public ActionResult EditHeading(Heading p)
+        {
+            hm.HeadingUpdate(p);
+            return RedirectToAction("Index");
+        }
 
+        public ActionResult DeleteHeading(int id)
+        {
+            var HeadingValue = hm.GetByID(id);
+            hm.HeadingDelete(HeadingValue);
+            return RedirectToAction("Index");
+        }
     }
 }
