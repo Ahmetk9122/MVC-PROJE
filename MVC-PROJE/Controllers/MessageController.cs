3326c4fff9418fb10698f06fd66f9d949bd7d61e
Ahmet KARABUDAK <ahmet.karabudakk.9122@gmail.com>
2021-06-02 22:17:56 +0300

    Message Controller Added.

---
+++ MVC-PROJE/Controllers/MessageController.cs
@@ -0,0 +1,23 @@
+﻿
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_PROJE.Controllers
+{
+    public class MessageController : Controller
+    {
+
+        MessageManager cm = new MessageManager(new EFMessageDal());
+        // GET: Message
+        public ActionResult Inbox()
+        {
+            var messagelist = cm.GetList();
+            return View(messagelist);
+        }
+    }
+}
