b98f67f8ab5d8cfb7a8942b4243eff831a0a242d
Ahmet KARABUDAK <ahmet.karabudakk.9122@gmail.com>
2021-06-02 22:16:41 +0300

    Message Interface added.

---
+++ BusinessLayer/Abstracy/IMessageService.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstracy
+{
+    public interface IMessageService
+    {
+        List<Message> GetList();
+        void MessageAdd(Message message);
+        Message GetByID(int id);
+
+        void MessageDelete(Message message);
+
+        void MessageUpdate(Message message);
+    }
+}
