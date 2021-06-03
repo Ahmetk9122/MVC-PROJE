c55c7b33e034e04b9fea2dfa1e7b5d1ec785e70e
Ahmet KARABUDAK <ahmet.karabudakk.9122@gmail.com>
2021-06-03 17:58:32 +0300

    New message and ListSendbox Added

--- MVC-PROJE/Controllers/MessageController.cs
+++ MVC-PROJE/Controllers/MessageController.cs
@@ -1,6 +1,7 @@
 ﻿
 using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrate;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,8 +17,24 @@ namespace MVC_PROJE.Controllers
         // GET: Message
         public ActionResult Inbox()
         {
-            var messagelist = cm.GetList();
+            var messagelist = cm.GetListInbox();
             return View(messagelist);
+        }
+        public ActionResult Sendbox()
+        {
+            var messagelist = cm.GetListSendbox();
+            return View(messagelist);
+        }
+        [HttpGet]
+        public ActionResult NewMessage ()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
+        {
+
+            return View();
         }
     }
 }
