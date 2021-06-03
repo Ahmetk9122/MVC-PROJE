9c7808aec927d26d8f092be21e90cc51f0d20aca
Ahmet KARABUDAK <ahmet.karabudakk.9122@gmail.com>
2021-06-03 17:57:54 +0300

    Message Validator Created

---
+++ BusinessLayer/ValidationRules/MessageValidator.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrate;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class MessageValidator : AbstractValidator<Message>
+    {
+        public MessageValidator()
+        {
+            RuleFor(x => x.ReceiverMail).NotEmpty().WithMessage("Alıcı adresini boş geçemezsiniz");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Konuyu adresini boş geçemezsiniz");
+            RuleFor(x => x.MessageContent).NotEmpty().WithMessage("Mesajı adresini boş geçemezsiniz");
+            //Girilen Mail adresi gerçek mail adresi olsun
+            RuleFor(x => x.Subject).MinimumLength(3).WithMessage("Lütfen En Az 3 Karakter Girişi Yapın.");
+            RuleFor(x => x.Subject).MaximumLength(100).WithMessage("Lütfen 100 Karakterden Fazla Değer Girişi Yapmayın.");
+
+
+        }
+    }
+}
