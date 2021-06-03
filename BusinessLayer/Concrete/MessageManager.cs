68631bb7d32deb1f878f911eda17a2310b32eaef
Ahmet KARABUDAK <ahmet.karabudakk.9122@gmail.com>
2021-06-03 17:57:29 +0300

    Message Interface and Manager Edited(SenderMail ReceiverMail)

--- BusinessLayer/Concrete/MessageManager.cs
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -23,15 +23,21 @@ namespace BusinessLayer.Concrete
             throw new NotImplementedException();
         }
 
-        public List<Message> GetList()
+        public List<Message> GetListInbox()
         {
             //admin@gmail.com buraya gönderilen mesajları listelemeye yarayacak.
             return _messageDal.List(x => x.ReceiverMail == "admin@gmail.com");
         }
 
+        public List<Message> GetListSendbox()
+        {
+            return _messageDal.List(x => x.SenderMail == "admin@gmail.com");
+
+        }
+
         public void MessageAdd(Message message)
         {
-            throw new NotImplementedException();
+            _messageDal.Insert(message);
         }
 
         public void MessageDelete(Message message)
@@ -43,5 +49,6 @@ namespace BusinessLayer.Concrete
         {
             throw new NotImplementedException();
         }
+
     }
 }
