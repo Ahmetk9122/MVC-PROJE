e10f5aee06df56af3b67fb8cbb23b471f434d4b6
Ahmet KARABUDAK <ahmet.karabudakk.9122@gmail.com>
2021-06-05 23:44:13 +0300

    404 error page created.

--- MVC-PROJE/Controllers/MessageController.cs
+++ MVC-PROJE/Controllers/MessageController.cs
@@ -1,7 +1,9 @@
 ﻿
 using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrate;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +16,7 @@ namespace MVC_PROJE.Controllers
     {
 
         MessageManager cm = new MessageManager(new EFMessageDal());
+        MessageValidator messagevalidator = new MessageValidator();
         // GET: Message
         public ActionResult Inbox()
         {
@@ -30,6 +33,13 @@ namespace MVC_PROJE.Controllers
             var values = cm.GetByID(id);
             return View(values);
         }
+
+        public ActionResult GetSendBoxMessageDetails(int id)
+        {
+            var values = cm.GetByID(id);
+            return View(values);
+        }
+
         [HttpGet]
         public ActionResult NewMessage ()
         {
@@ -38,7 +48,21 @@ namespace MVC_PROJE.Controllers
         [HttpPost]
         public ActionResult NewMessage(Message p)
         {
-
+            ValidationResult result = messagevalidator.Validate(p);
+            if (result.IsValid)
+            {
+                p.MessageDate=DateTime.Parse(DateTime.Now.ToShortDateString());
+                cm.MessageAdd(p);
+                return RedirectToAction("SendBox");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
             return View();
         }
     }
