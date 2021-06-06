7e7532d4c3239a3fedd919fe1b659a68c261ec8c
Ahmet KARABUDAK <ahmet.karabudakk.9122@gmail.com>
2021-06-06 19:39:19 +0300

    Image service and manager added.

---
+++ BusinessLayer/Concrete/ImageFileManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstracy;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+   public class ImageFileManager : IimageFileService
+    {
+        IimageFileDal _imagefileDal;
+
+        public ImageFileManager(IimageFileDal imagefileDal)
+        {
+            _imagefileDal = imagefileDal;
+        }
+
+        public List<imageFile> GetList()
+        {
+           return  _imagefileDal.List();
+        }
+    }
+}
