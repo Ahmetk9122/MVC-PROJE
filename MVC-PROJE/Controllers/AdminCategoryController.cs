ff444d2d4cfde1a86c3cb8cbd4798f93958dbfb6
Ahmet KARABUDAK <ahmet.karabudakk.9122@gmail.com>
2021-06-09 17:16:13 +0300

    Admin Categry Controller Update

--- MVC-PROJE/Controllers/AdminCategoryController.cs
+++ MVC-PROJE/Controllers/AdminCategoryController.cs
@@ -15,7 +15,7 @@ namespace MVC_PROJE.Controllers
     {
         CategoryManager cm = new CategoryManager(new EfCategoryDal());
 
-        [Authorize]
+        [Authorize(Roles="B")]
         public ActionResult Index()
         {
             var categoryvalues = cm.GetList();
