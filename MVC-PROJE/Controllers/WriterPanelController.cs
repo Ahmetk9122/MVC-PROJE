44e2017aaf5f8a1f493f6057da2cfb9019f921b7
Ahmet KARABUDAK <ahmet.karabudakk.9122@gmail.com>
2021-06-20 23:47:27 +0300

    Edit heading new heading and my heading list edited.

--- MVC-PROJE/Controllers/WriterPanelController.cs
+++ MVC-PROJE/Controllers/WriterPanelController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrate;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace MVC_PROJE.Controllers
     public class WriterPanelController : Controller
     {
         HeadingMenager hm = new HeadingMenager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
         // GET: WriterPanel
         public ActionResult WriterProfile()
         {
@@ -24,5 +26,50 @@ namespace MVC_PROJE.Controllers
             return View(values);
 
         }
+        [HttpGet]
+        public ActionResult NewHeading()
+        {
+            //Dropdown olayının backend kısmı 
+            List<SelectListItem> valueCategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            ViewBag.vlc = valueCategory;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.WriterID = 4;
+            p.HeadingStatus = true;
+            hm.HeadingAdd(p);
+            return RedirectToAction("MyHeading");
+        }
+        [HttpGet]
+        public ActionResult EditHeading(int id)
+        {
+            List<SelectListItem> valueCategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            var HeadingValue = hm.GetByID(id);
+            ViewBag.vlc = valueCategory;
+            return View(HeadingValue);
+        }
+        [HttpPost]
+        public ActionResult EditHeading(Heading p)
+        {
+            hm.HeadingUpdate(p);
+            return RedirectToAction("MyHeading");
+        }
+
     }
 }
+// < customErrors mode = "On" >
+//           < error statusCode = "404" redirect = "/ErrorPage/Page404" />
+//          </ customErrors >
