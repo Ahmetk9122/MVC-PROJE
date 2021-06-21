a362fe87a4c9e97ed48a3c53ecc793692e919959
Ahmet KARABUDAK <ahmet.karabudakk.9122@gmail.com>
2021-06-21 12:28:40 +0300

    Send Inbox Added

---
+++ MVC-PROJE/Controllers/WriterPanelMessageController.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_PROJE.Controllers
+{
+    public class WriterPanelMessageController : Controller
+    {
+
+        MessageManager cm = new MessageManager(new EFMessageDal());
+        MessageValidator messagevalidator = new MessageValidator();
+        // GET: WriterPanelMessage
+        public ActionResult Inbox()
+        {
+            var messagelist = cm.GetListInbox();
+            return View(messagelist);
+        }
+        public ActionResult Sendbox()
+        {
+            var messagelist = cm.GetListSendbox();
+            return View(messagelist);
+        }
+        public PartialViewResult MessageListMenu()
+        {
+            return PartialView();
+        }
+
+    }
+}
