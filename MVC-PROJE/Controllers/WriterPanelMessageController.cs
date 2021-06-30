04a9ba415e65196e0da784ac347b6c7d05cfdf13
Ahmet KARABUDAK <ahmet.karabudakk.9122@gmail.com>
2021-06-30 16:43:48 +0300

    Inbox and Send Box Message Details added Also New Message controller added.

--- MVC-PROJE/Controllers/WriterPanelMessageController.cs
+++ MVC-PROJE/Controllers/WriterPanelMessageController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
 using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrate;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +31,44 @@ namespace MVC_PROJE.Controllers
         {
             return PartialView();
         }
+
+        public ActionResult GetInboxMessageDetails(int id)
+        {
+            var values = cm.GetByID(id);
+            return View(values);
+        }
+
+        public ActionResult GetSendBoxMessageDetails(int id)
+        {
+            var values = cm.GetByID(id);
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
+        {
+            ValidationResult result = messagevalidator.Validate(p);
+            if (result.IsValid)
+            {
+                p.SenderMail = "Gizem@gmail.com";
+                p.MessageDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                cm.MessageAdd(p);
+                return RedirectToAction("SendBox");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
 
     }
 }
